bb23e594b6744f7e2a3f55892fd732f9306340d5
B.M. Ferdous Mahmud <mahmudproject89@gmail.com>
2024-05-03 15:02:58 +0600

    clean project structure created

---
+++ lib/core/constant/api_constants.dart
@@ -0,0 +1,8 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const baseApiUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com';
+  static const commonExtUrl = '/recipes';
+  static const searchUrl = '/complexSearch';
+  static const detailsUrl = '/information';
+}
