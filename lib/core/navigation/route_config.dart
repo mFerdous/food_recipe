4bb1f5665f8037a3c9fde095a4740691d0e56122
B.M. Ferdous Mahmud <mahmudproject89@gmail.com>
2024-05-03 15:07:17 +0600

    Squashed commit of the following:
    commit bb23e594b6744f7e2a3f55892fd732f9306340d5
    Author: B.M. Ferdous Mahmud <mahmudproject89@gmail.com>
    Date:   Fri May 3 15:02:58 2024 +0600
    
        clean project structure created
    
    clean project architecture merged

---
+++ lib/core/navigation/route_config.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../../features/home/presentation/screens/home_page.dart';
+import 'route_name.dart';
+
+class RouteConfig {
+  Route routes(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case RouteName.initialRoute:
+        return _getInitialRoute();
+    }
+    return _defaultRoute();
+  }
+
+  static MaterialPageRoute _routeBuilder(Widget child) {
+    return MaterialPageRoute(builder: (_) => child);
+  }
+
+  static MaterialPageRoute _defaultRoute() {
+    return _routeBuilder(
+      const Scaffold(
+        body: Center(
+          child: Text('Unknown route'),
+        ),
+      ),
+    );
+  }
+
+  static MaterialPageRoute _getInitialRoute() {
+    return _routeBuilder(const HomePage());
+    // return _routeBuilder(const SplashScreen());
+  }
+}
